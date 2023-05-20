def0ad9bae5f66320301fcc32e7593667bb4ab51
quist <joakiml@gmail.com>
2023-05-20 09:01:33 +0200

    Add check if input file exists

--- src/utils/argumentParser.ts
+++ src/utils/argumentParser.ts
@@ -1,10 +1,28 @@
+import * as fs from "fs";
+
 export class ArgumentParser {
   static parseArguments(): ParsedArguments {
     if (process.argv.length <= 2) {
       throw new Error("Usage: input output");
     }
     console.log(process.argv);
-    return { inputFilePath: process.argv[2] }; // To do: check if file exists.
+    const inputFilePath = process.argv[2];
+    if (!ArgumentParser.fileExist(inputFilePath)) {
+      throw new Error(
+        `Could not read input file with path '${inputFilePath}'. Does it exist?`
+      );
+    }
+    return { inputFilePath: inputFilePath };
+  }
+
+  private static fileExist(path: string) {
+    try {
+      if (fs.existsSync(path)) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
   }
 }
 
