253a35f61a7f2b92f3f3ffa24466de8dcced148b
quist <joakiml@gmail.com>
2023-05-21 22:02:42 +0200

    Post file to backend
    Also, add prettier.

--- shiftworker-web/src/App.tsx
+++ shiftworker-web/src/App.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Upload iCal file to get started.
-        </p>
-        <input type="file"
-               id="avatar" name="avatar"
-               onChange={(e) => {
-                 if (!e.target.files) {
-                   return
-                 }
-                 const reader = new FileReader()
-                 reader.onload = async (e) => {
-                   const text = (e.target?.result)
-                   console.log(text)
-                   alert(text)
-                 };
-                 reader.readAsText(e.target.files[0])
-               }}
-               >
-        </input>
+        <p>Upload iCal file to get started.</p>
+        <input
+          type="file"
+          id="avatar"
+          name="avatar"
+          onChange={(e) => {
+            if (!e.target.files) {
+              return;
+            }
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+              const text = e.target?.result;
+              console.log(text);
+              postToBackend(text as string);
+            };
+            reader.readAsText(e.target.files[0]);
+          }}
+        ></input>
       </header>
     </div>
   );
 }
+
+const postToBackend = async (payload: string) => {
+  await fetch(
+    "https://us-central1-shiftworker-387320.cloudfunctions.net/shiftworkerHttp",
+    { method: "post", body: payload }
+  );
+};
 
 export default App;
