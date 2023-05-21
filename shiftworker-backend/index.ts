d6ec6d9c50bc82c9b71ec17c0b688f8ffcb827b0
quist <joakiml@gmail.com>
2023-05-21 22:02:52 +0200

    Enable CORS

--- shiftworker-backend/index.ts
+++ shiftworker-backend/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "@google-cloud/functions-framework";
 
-const functions = require('@google-cloud/functions-framework');
+const functions = require("@google-cloud/functions-framework");
 
 /**
  * Responds to an HTTP request using data from the request body parsed according
@@ -9,6 +9,16 @@ const functions = require('@google-cloud/functions-framework');
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-functions.http('shiftworkerHttp', (req: Request, res: Response) => {
-	res.send(`Hello World!`);
+functions.http("shiftworkerHttp", (req: Request, res: Response) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+  } else {
+    res.send("Hello World!");
+  }
 });
