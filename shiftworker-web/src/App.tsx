3fbcfc034e74f70088619dd40708d1b6541f30e8
quist <joakiml@gmail.com>
2023-06-29 22:17:43 +0200

    Show loading and response when posting to backend

--- shiftworker-web/src/App.tsx
+++ shiftworker-web/src/App.tsx
@@ -1,37 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState<string | null>(null);
+
+  const onFileSelected = (files: FileList) => {
+    setIsLoading(true);
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const text = e.target?.result;
+      const result = await postToBackend(text as string);
+      setResponse(result);
+      setIsLoading(false);
+    };
+    reader.readAsArrayBuffer(files[0]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Upload iCal file to get started.</p>
-        <input
-          type="file"
-          id="avatar"
-          name="avatar"
-          onChange={(e) => {
-            if (!e.target.files) {
-              return;
-            }
-            const reader = new FileReader();
-            reader.onload = async (e) => {
-              const text = e.target?.result;
-              console.log(text);
-              postToBackend(text as string);
-            };
-            reader.readAsArrayBuffer(e.target.files[0]);
-          }}
-        ></input>
+        <div style={{ display: "flex" }}>
+          <FileInput onChange={onFileSelected} />
+          {isLoading && (
+            <img
+              src={logo}
+              className="App-logo"
+              style={{ height: 60, width: 60, alignSelf: "center" }}
+              alt="logo"
+            />
+          )}
+        </div>
+        {response && <p>{response}</p>}
       </header>
     </div>
   );
 }
 
-const postToBackend = async (payload: string) => {
-  await fetch(
+const FileInput = (props: { onChange: (files: FileList) => void }) => (
+  <input
+    type="file"
+    id="avatar"
+    name="avatar"
+    style={{ alignSelf: "center" }}
+    onChange={(e: React.FormEvent<HTMLInputElement>) => {
+      const input = e.target as HTMLInputElement;
+      if (!input.files) {
+        return;
+      }
+      props.onChange(input.files);
+    }}
+  />
+);
+
+const postToBackend = async (payload: string): Promise<string> => {
+  const response = await fetch(
     "https://us-central1-shiftworker-387320.cloudfunctions.net/shiftworkerHttp",
     {
       method: "post",
@@ -39,6 +65,7 @@ const postToBackend = async (payload: string) => {
       headers: { "Content-Type": "application/octet-stream" },
     }
   );
+  return await response.text();
 };
 
 export default App;
